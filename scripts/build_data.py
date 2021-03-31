a4b302b6208a65ca74e22a7d9cc57ed32482c004
Brock Fanning <brockfanning@gmail.com>
2021-03-31 09:21:33 -0400

    Fixes

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,11 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_data(df):
+    column_fixes = {
+        'Yeat': 'Year',
+        'السنة': 'Year',
+    }
+    df = df.rename(columns=column_fixes)
+    return df
+
+open_sdg_build(config='config_data.yml', alter_data=alter_data)
